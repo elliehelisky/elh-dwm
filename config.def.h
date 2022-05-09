0648e13151a81a157ceeeaa6ad694dbf5fc2afea
Ellie Helisky <92484654+elliehelisky@users.noreply.github.com>
2022-05-09 04:17:50 +0000

    Update config.def.h
    fixed path

--- config.def.h
+++ config.def.h
@@ -103,8 +103,7 @@ static Key keys[] = {
      SHCMD("nemo || st -e ranger || thunar "
            "|| dolphin || nautilus")},
     {MODKEY, XK_b, spawn,
-     SHCMD("/nix/store/29z6mphzfq1gy7042hx9cp5gmvbg52r1-librewolf-99.0.1-3/bin/"
-           "librewolf || firefox || chromium")},
+     SHCMD("librewolf || firefox || chromium")},
     {MODKEY, XK_x, spawn, SHCMD("slock")},
     {0, XK_Print, spawn,
      SHCMD("mkdir ~/Pictures/screenshots & pkill feh & scrot -s "
