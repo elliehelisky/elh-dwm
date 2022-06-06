a05213ec9646d243beb58b4c383f29be51e08278
Ellie <92484654+elliehelisky@users.noreply.github.com>
2022-06-06 01:15:58 +0000

    oops

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
-sudo make clean install || sudo config.h && sudo make clean install
+sudo make clean install || sudo rm config.h && sudo make clean install
 sudo mkdir -p /usr/share/xsessions/ || echo "xsessions folder already exist. Skipping...."
 sudo cp elh-dwm.desktop /usr/share/xsessions/ || echo "elh-dwm.desktop already exist. Skipping...."
 echo "Done."
