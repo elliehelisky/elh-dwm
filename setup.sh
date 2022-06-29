96aba012f54e0c5dd961d0f24ea4676329040b5f
Ellie <92484654+elliehelisky@users.noreply.github.com>
2022-06-29 13:58:36 -0500

    Removed sudo from commands
    This was bad practice. Instead, the script will ask for sudo permissions prior to running.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 !#/bin/bash
-
-sudo make clean install || sudo rm config.h && sudo make clean install
-sudo mkdir -p /usr/share/xsessions/ || echo "xsessions folder already exist. Skipping...."
-sudo cp elh-dwm.desktop /usr/share/xsessions/ || echo "elh-dwm.desktop already exist. Skipping...."
+if [ `whoami` != root ]; then
+    echo "Please run this script using sudo"
+    exit
+else
+make clean install || rm config.h && make clean install
+mkdir -p /usr/share/xsessions/ || echo "xsessions folder already exist. Skipping...."
+cp elh-dwm.desktop /usr/share/xsessions/ || echo "elh-dwm.desktop already exist. Skipping...."
 echo "Done."
+fi
